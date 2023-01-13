71a0720050597e34fc8e7e07a0c13cd49a550e63
Guillaume-Viale <119675245+Guillaume-Viale@users.noreply.github.com>
2023-01-13 14:26:03 +0100

    Add files via upload

---
+++ RETR.py
@@ -0,0 +1,22 @@
+import pygame
+from pygame.locals import *
+
+pygame.init()
+fenetre = pygame.display.set_mode((700, 500))
+x=25
+y=25
+boucle=1
+fond=pygame.image.load("pixil-frame-0(5).png").convert()
+fenetre.blit(fond, (0,0))
+pierre=pygame.image.load("PIERRE.png").convert_alpha()
+pygame.display.flip()
+fenetre.blit(pierre,(20,20))
+pygame.display.flip()
+#personnage=pygame.image.load("MAIN_CHARACTER.gif").convert_alpha()
+#fenetre.blit(personnage(x,y))
+
+while boucle:
+    for event in pygame.event.get():
+
+        if event.type == QUIT:
+            boucle = 0
