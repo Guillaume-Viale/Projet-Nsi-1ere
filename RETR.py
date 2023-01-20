e0e92c70104c620e7d52766571ad463d4c2722bc
yannicktamko <119676121+yannicktamko@users.noreply.github.com>
2023-01-20 13:46:21 +0100

    Update RETR.py

--- RETR.py
+++ RETR.py
@@ -9,20 +9,23 @@ y=25
 boucle=1
 fond=pygame.image.load("pixil-frame-0(5).png").convert()
 fenetre.blit(fond, (0,0))
-pierre=pygame.image.load("PIERRE.png").convert_alpha()
+#pierre=pygame.image.load("PIERRE.png").convert_alpha()
 pygame.display.flip()
-fenetre.blit(pierre,(20,20))
+#fenetre.blit(pierre,(20,20))
 pygame.display.flip()
 #personnage=pygame.image.load("MAIN_CHARACTER.gif").convert_alpha()
 #fenetre.blit(personnage(x,y))
-Class sound:
-mixer.init()
-mixer.music.load("")
-mixer.music.set_volume(0.7)
-mixer.music.play()    
+#Class sound:
+pygame.mixer.init()
+pygame.mixer.music.load("RETRO-ADVENTURE-MUSIC.wav")
+pygame.mixer.music.set_volume(3.0)
+pygame.mixer.music.play(loops=-1)
+
+
 
 while boucle:
     for event in pygame.event.get():
 
         if event.type == QUIT:
             boucle = 0 
+
