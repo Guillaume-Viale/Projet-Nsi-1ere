a5a3f9a75edbb9061d6992f71a396378dadc82e9
Guillaume-Viale <119675245+Guillaume-Viale@users.noreply.github.com>
2023-02-10 13:15:15 +0100

    Update RETR.py

--- Retro Adventure/RETR.py
+++ Retro Adventure/RETR.py
@@ -19,7 +19,16 @@ pygame.display.flip()
 x_movement=20
 y_movement=20
 FPS = 60
+white = (255, 255, 255)
+black = (0, 0, 0)
+MouseD = False
+inMenu = True
+GameOver = False
 clock = pygame.time.Clock()
+font=pygame.font.Font('freesansbold.ttf', 32)
+Title=font.render('RETRO ADVENTURE',True, white,black)
+starttext=font.render('CLICK TO START', True, white, black)
+gameover=font.render('GAME OVER',True,white,black)
 #personnage=pygame.image.load("MAIN_CHARACTER.gif").convert_alpha()
 #fenetre.blit(personnage(x,y))
 def handle_keys(keys: list, pos: pygame.Rect):
@@ -51,15 +60,24 @@ def handle_keys(keys: list, pos: pygame.Rect):
         pos.x += 2
 def draw(pos: pygame.Rect):
     # Fond
-      
-    fenetre.blit(fond, (0,0))
-    # Blit player
-    fenetre.blit(pierre, (pos[0],pos[1]))
-    for i in range (-450,350,45):
-        fenetre.blit(sol, (i,125))  
-    # Update
+    if inMenu:
+        fenetre.blit(fondMain,(0,0))
+        fenetre.blit(starttext, (250,350))
+        fenetre.blit(Title,(250,100)
+    if inMenu == False:
+        fenetre.blit(fond, (0,0))
+   
+        fenetre.blit(pierre, (pos[0],pos[1]))
+        for i in range (-450,350,45):
+            fenetre.blit(sol, (i,125))  
+         if GameOver == True:
+            fenetre.blit(fondMain,(0,0))
+            inMenu = True
+            fenetre.blit(gameover,(250,100))
+            pygame.display.flip
+                  
     pygame.display.update()
-def main():
+def main(inMenu,GameOver):
     PLAYER_RECT = pierre.get_rect(center=(
         fenetre.get_width()//2,
         fenetre.get_height()//2
@@ -71,22 +89,33 @@ def main():
 
         # Event handler
         for event in pygame.event.get():
+            if pygame.mouse.get_pressed()[0]:
+                inMenu = False
+                MouseD = True
+            else:
+                MouseD = False
+                
+            
             if event.type == pygame.QUIT:
                 run = False
                 
         KEYS_PRESSED = pygame.key.get_pressed()
+        if KEYS_PRESSED[pygame.K_q]:
+            GameOver = True
+            
         # Handle the keys
         handle_keys(KEYS_PRESSED, PLAYER_RECT)
         # Call draw function
-        draw(PLAYER_RECT)
+        draw(PLAYER_RECT,inMenu,GameOver)
+        print(MouseD,inMenu,GameOver)
+        
 #Class sound:
 pygame.mixer.init()
 pygame.mixer.music.load("RETRO-ADVENTURE-MUSIC.wav")
 pygame.mixer.music.set_volume(3.0)
 pygame.mixer.music.play(loops=-1)
     # Quit
+#pygame.quit()
 if __name__ == '__main__':
-    main()
-    
-pygame.quit() 
-   
+    main(inMenu,GameOver)
+pygame.quit()
