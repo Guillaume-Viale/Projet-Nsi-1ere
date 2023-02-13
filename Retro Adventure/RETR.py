24ceaa5c2a08787c1d73fe508fd06def33fd0500
Guillaume-Viale <119675245+Guillaume-Viale@users.noreply.github.com>
2023-02-13 17:47:24 +0100

    Update RETR.py

--- Retro Adventure/RETR.py
+++ Retro Adventure/RETR.py
@@ -14,10 +14,17 @@ fond=pygame.image.load("CielFinal.20.png").convert()
 fond=pygame.transform.scale(fond, (700,500))
 fondMain=pygame.image.load("FondMainmenu.jpg").convert()
 fondMain=pygame.transform.scale(fondMain,(700,500))
-sol=pygame.image.load("pixil-frameHerbe.png").convert_alpha()
+sol_herbe=pygame.image.load("pixil-frameHerbe.png").convert_alpha()
+sol_terre=pygame.image.load("TerreFinal700500.png").convert_alpha()
+sol_terre=pygame.transform.scale(sol_terre,(675,482))
 pierre=pygame.image.load("pixil-frameStatique1.png").convert_alpha()
 vie1=pygame.image.load("Coeur_Heros.png").convert_alpha()
 vie2=pygame.image.load("CoeurPerduHeros.png").convert_alpha()
+#balle=pygame.image.load("").convert_alpha()
+#mechant1=pygame.image.load("").convert_alpha()
+#mechant2=pygame.image.load("").convert_alpha()
+#mechant3=pygame.image.load("").convert_alpha()
+#mechant4=pygame.image.load("").convert_alpha()
 
 #GAMEPLAY
 score = '0'
@@ -35,26 +42,27 @@ gameover=font.render('GAME OVER',True,white,black)
 
 #PAS_CHANGER
 FPS = 60
+time = 0
 MouseD = False
 inMenu = True
 GameOver = False
 clock = pygame.time.Clock()
-
+tirs_liste = []
 
 def handle_keys(keys: list, pos: pygame.Rect):
     if pos.y == 100:
         state= True
     else:
         state= False
-        
+
     if pos.y != 100:
         pos.y += 10
     if state==True :
-        if keys[pygame.K_SPACE]:
-            # If space is pressed
+        if keys[pygame.K_UP]:
+            #FLECHE VERS LE HAUT POUR SAUTER
             for i in range(100):
                 pos.y-=1
-         
+
     #MOUVEMENT HORIZONTALE(FLECHES)
     if keys[pygame.K_LEFT]:
         pos.x -= 5
@@ -89,14 +97,19 @@ def draw(pos: pygame.Rect,inMenu,GameOver,scoreTexte,vie):
             pygame.display.flip
         fenetre.blit(pierre, (pos[0],pos[1]))
         for i in range (-450,350,45):
-            fenetre.blit(sol, (i,125))  
+            fenetre.blit(sol_herbe, (i,125))
+            fenetre.blit(sol_terre, (i+50,175))
+            fenetre.blit(sol_terre, (i+50,205))
+            fenetre.blit(sol_terre, (i+50,235))
         fenetre.blit(scoreTexte,(600,40))
+        for tir in tirs_liste:
+            fenetre.blit(vie1,(tir[0],tir[1]))
         if GameOver == True:
             inMenu = True
             fenetre.blit(fondMain,(0,0))
             fenetre.blit(gameover,(230,500))
             pygame.display.flip
-                  
+
     pygame.display.update()
 
 def main(inMenu,GameOver,vie,score):
@@ -108,7 +121,7 @@ def main(inMenu,GameOver,vie,score):
     while run:
         # Tick at n FPS
         clock.tick(FPS)
-
+        time = clock.get_time
         # Event handler
         for event in pygame.event.get():
             if pygame.mouse.get_pressed()[0]:
@@ -116,11 +129,11 @@ def main(inMenu,GameOver,vie,score):
                 MouseD = True
             else:
                 MouseD = False
-                
-            
+
+
             if event.type == pygame.QUIT:
                 run = False
-                
+
         KEYS_PRESSED = pygame.key.get_pressed()
         if KEYS_PRESSED[K_q]:
             vie -= 1
@@ -132,18 +145,25 @@ def main(inMenu,GameOver,vie,score):
         if KEYS_PRESSED[pygame.K_ESCAPE]:
             pygame.quit()
             run = False
+
+        if KEYS_PRESSED[pygame.K_SPACE] and int(clock.get_rawtime())%2 == 0:
+            tirs_liste.append([PLAYER_RECT.x +4, PLAYER_RECT.y +4])
+        for tir in tirs_liste:
+            tir[0] += 20
+            if 0>tir[0]>700:
+                tirs_liste.remove(tir)
         # Handle the keys
         handle_keys(KEYS_PRESSED, PLAYER_RECT)
         # Call draw function
         draw(PLAYER_RECT,inMenu,GameOver,scoreTexte,vie)
         print(vie,score)
-        
+
 #Class sound:
 pygame.mixer.init()
 pygame.mixer.music.load("RETRO-ADVENTURE-MUSIC.wav")
 pygame.mixer.music.set_volume(3.0)
 pygame.mixer.music.play(loops=-1)
-    
+
 if __name__ == '__main__':
     main(inMenu,GameOver,vie,score)
 pygame.quit()
